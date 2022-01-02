c0c46ae0793d151a9979056060cf1baf53b6409c
Sangsaka Wira <sangsakawira@gmail.com>
2022-01-02 10:01:32 +0700

    first commit

---
+++ pages/about/index.js
@@ -0,0 +1,11 @@
+import Navbar from "../components/Navbar";
+
+export default function About(){
+    return(
+        <>
+        <Navbar current={"About"}></Navbar>
+        <h1>About</h1>
+        </>
+        
+    )
+}
---
+++ pages/home/index.js
@@ -0,0 +1,11 @@
+import Navbar from "../components/Navbar";
+
+export default function Home(){
+    return(
+        <>
+        <Navbar current={"Home"}></Navbar>
+        <h1>Home</h1>
+        </>
+        
+    )
+}
