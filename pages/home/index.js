c8be99d2fd061d9c4e5eaf0bb921476921027847
Sangsaka Wira <sangsakawira@gmail.com>
2022-01-02 13:02:41 +0700

    add data

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,11 +1,23 @@
 import Navbar from "../components/Navbar";
 
-export default function Home(){
-    return(
+export default function Home() {
+    return (
         <>
-        <Navbar current={"Home"}></Navbar>
-        <h1>Home</h1>
+            <Navbar current={"Home"}></Navbar>
+            <div class="container mx-auto">
+                {["Hari Ini", "Malam Minggu"].map(doc => {
+                    return (
+                        <div className="mt-6">
+                            <div className="bg-gradient-to-r from-cyan-500 to-blue-500 p-6 max-w-full mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4 w-full">
+                                <div>
+                                    <div className="text-xl font-medium text-white">{doc}</div>
+                                    <p className="text-white">You have a new message!</p>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
         </>
-        
     )
 }
